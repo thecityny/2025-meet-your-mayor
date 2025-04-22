4ad79bceb15ab4dd5d5440371b60e67cff0430c3
Sam Rabiyah <sraby@users.noreply.github.com>
2025-04-22 18:13:13 -0400

    Add donation buttons next to related coverage

--- src/components/RecentCoverage.tsx
+++ src/components/RecentCoverage.tsx
@@ -2,32 +2,44 @@ import React from "react";
 import { coverageLinksTheCity } from "../coverage-links";
 import { OutboundLink } from "./Links";
 
+const GOTHAMIST_DONATION_URL =
+  "https://pledge.wnyc.org/support/gothamist/?utm_medium=partnersite&utm_source=the-city&utm_campaign=meet-your-mayor";
+
+const THE_CITY_DONATION_URL =
+  "https://donorbox.org/nycdonate?utm_source=button&utm_medium=website&utm_campaign=meet%20your%20mayor%202025";
+
 export const RecentCoverage: React.FC = () => (
   <>
-    <div className="mt-3">THE CITY</div>
+    <div className="eyebrow mt-3">THE CITY</div>
     <ul>
       {coverageLinksTheCity.map((link, i) => (
         <li key={i} className="is-inline-block mb-1">
-          •{" "}
-          <OutboundLink to={link.href} className="has-text-left">
+          ●{" "}
+          <OutboundLink to={link.href} className="copy has-text-left">
             {link.text}
           </OutboundLink>
         </li>
       ))}
     </ul>
-    <div className="mt-3">GOTHAMIST</div>
+    <OutboundLink to={THE_CITY_DONATION_URL}>
+      <div className="button is-outlined is-small mt-3">Donate</div>
+    </OutboundLink>
+    <div className="eyebrow mt-6 pt-3">GOTHAMIST</div>
     <ul>
       {
         // TODO: Replace with actual Gothamist links
         coverageLinksTheCity.map((link, i) => (
           <li key={i} className="is-inline-block mb-1">
-            •{" "}
-            <OutboundLink to={link.href} className="has-text-left">
+            ●{" "}
+            <OutboundLink to={link.href} className="copy has-text-left">
               {link.text}
             </OutboundLink>
           </li>
         ))
       }
     </ul>
+    <OutboundLink to={GOTHAMIST_DONATION_URL}>
+      <div className="button is-outlined is-small mt-3">Donate</div>
+    </OutboundLink>
   </>
 );
