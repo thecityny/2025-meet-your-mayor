3b835c0088e768af151210a85834568c3009faa2
Sam Rabiyah <sraby@users.noreply.github.com>
2025-05-06 11:15:40 -0400

    Save appstore as storage, and convert favoriteTopics to array

--- src/useAppStore.ts
+++ src/useAppStore.ts
@@ -1,14 +1,14 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 import { QuizInput, ScoreCard } from './components/QuizContent';
 
 export type Party = "democrat" | "other" | null;
 
-
 type AppState = {
   party: Party;
   setParty: (party: Party) => void;
-  favoriteTopics: Set<string>;
-  setFavoriteTopics: (favoriteTopics: Set<string>) => void;
+  favoriteTopics: string[];
+  setFavoriteTopics: (favoriteTopics: string[]) => void;
   answers: QuizInput[];
   setAnswers: (answers: QuizInput[]) => void;
   score: ScoreCard | null;
@@ -21,10 +21,11 @@ type AppState = {
   setHighestVisibleQuestion: (highestVisibleQuestion: number) => void;
 };
 
-export const useAppStore = create<AppState>((set) => ({
+
+export const useAppStore = create<AppState>()(persist((set) => ({
   party: null,
   setParty: (party) => set({ party }),
-  favoriteTopics: new Set(),
+  favoriteTopics: [],
   setFavoriteTopics: (favoriteTopics) => set({favoriteTopics}),
   answers: [],
   setAnswers: (answers) => set({answers}),
@@ -32,4 +33,7 @@ export const useAppStore = create<AppState>((set) => ({
   setScore: (score) => set({score}),
   highestVisibleQuestion: 0,
   setHighestVisibleQuestion: (highestVisibleQuestion) => set({highestVisibleQuestion})
+}), {
+  name: 'app-store',
+  version: 1,
 }));
