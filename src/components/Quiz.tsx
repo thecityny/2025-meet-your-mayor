5348317b4a14d8a04fac31728f07ba0890dd927b
Sam Rabiyah <sraby@users.noreply.github.com>
2025-05-11 11:53:51 -0400

    Use scroller.scrollTo, instead of AnchorLink

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -15,6 +15,7 @@ import { abbreviateName, MatchingCandidates } from "./MatchingCandidates";
 import { Bobblehead } from "./Illustration";
 import { Party, useAppStore } from "../useAppStore";
 import { Methodology } from "./Methodology";
+import { scroller } from "react-scroll";
 
 export const CircleIcon: FC<{ filledIn?: boolean }> = ({ filledIn }) => (
   <div
@@ -174,22 +175,33 @@ const Quiz = () => {
 
                     {partySelectorButtons.map((button, i) => (
                       <div key={i} className="mt-5 mb-4">
-                        <SmoothScroll
-                          to="questions"
+                        <button
                           className="control"
                           onClick={() => {
                             setMethodologyVisible(false);
 
+                            setTimeout(() => {
+                              scroller.scrollTo("question-1", {
+                                duration: ANCHOR_LINK_DURATION,
+                                delay: 0,
+                                smooth: true,
+                                offset: QUESTION_ANCHOR_LINK_OFFSET, // optional, to adjust for headers etc.
+                              });
+                            }, 100); // wait until content has re-rendered
+
                             // If the user is selecting a party, we want to scroll to the first question
                             // after a short delay, so that the user doesn't see the content change
                             // inside the quiz intro section
-                            setTimeout(() => {
-                              setParty(button.party, ANCHOR_LINK_DURATION);
-                            }, 1000);
+
+                            setParty(button.party, ANCHOR_LINK_DURATION);
                           }}
-                          extraOffset={150}
                         >
-                          <button className="button">{button.label}</button>
+                          <div
+                            className="button"
+                            onClick={() => setMethodologyVisible(false)}
+                          >
+                            {button.label}
+                          </div>
                           <div className="is-flex is-flex-wrap-wrap is-flex-direction-row is-align-items-center my-3">
                             {button.party === "other" && (
                               <span className="copy is-inline-block m-0 mr-2">
@@ -213,22 +225,22 @@ const Quiz = () => {
                                 </div>
                               </div>
                             ))}
-                          </div>{" "}
-                        </SmoothScroll>
+                          </div>
+                        </button>
                       </div>
                     ))}
                   </>
                 )}
               </div>
               <div className="mb-5">
-                <span
+                <button
                   key="x"
                   className="eyebrow is-link is-inline-block"
                   onClick={() => toggleMethodology()}
                 >
                   How Meet Your Mayor Works{" "}
                   <span>{methodologyVisible ? "-" : "+"}</span>
-                </span>
+                </button>
 
                 {methodologyVisible && <Methodology />}
               </div>
