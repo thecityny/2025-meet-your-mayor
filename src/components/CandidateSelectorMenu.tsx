5064efc69501b92c6ebd9ecf845f4bf1bb139355
Sam Rabiyah <sraby@users.noreply.github.com>
2025-05-12 15:19:27 -0400

    Show busts only in candidate selector menu

--- src/components/CandidateSelectorMenu.tsx
+++ src/components/CandidateSelectorMenu.tsx
@@ -31,8 +31,8 @@ export const CandidateSelectorMenu: React.FC = () => {
           <div className="is-flex is-flex-direction-column is-align-items-center">
             <Bobblehead
               candidateName={candidate.name}
-              size={isCandidatePage ? "is-64x64" : "is-128x128"}
-              showBustOnly={isCandidatePage}
+              size={isCandidatePage ? "is-64x64" : "is-96x96"}
+              showBustOnly
               startAnimationRightAway
             />
 
