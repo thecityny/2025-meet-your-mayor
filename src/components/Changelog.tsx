8cf5ebccebb5c13a66139b037fc6d12a9822ece4
Sam Rabiyah <sraby@users.noreply.github.com>
2025-05-21 14:10:14 -0400

    Add a changelog component to top of quiz page (#143)

---
+++ src/components/Changelog.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+/**
+ * An array of objects, which include a date in the format like "May 21, 2025",
+ * and an array of React fragments that describe changes to the quiz.
+ *
+ * NOTE: Please use the <OutboundLink> component for hyperlinks.
+ */
+const CHANGELOG_ENTRIES = [
+  {
+    date: "May 21, 2025",
+    changes: [
+      <>
+        We added Jim Walden’s survey responses, submitted after Meet Your Mayor
+        launched.
+      </>,
+      <>
+        Brad Lander changed his response on the Rikers Island question (#6) and
+        added a response on public housing (#8).
+      </>,
+    ],
+  },
+];
+
+export const Changelog = () => (
+  <div className="my-3">
+    {CHANGELOG_ENTRIES.map((entry, i) => (
+      <div key={i}>
+        {entry.date}
+        <ul className="mt-1">
+          {entry.changes.map((change, i) => (
+            <li className="label is-flex my-0" key={i}>
+              <div className="mr-2 mt-1">●</div>{" "}
+              <span className="copy has-text-left ml-0 mb-1">{change}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    ))}
+  </div>
+);
--- src/components/Methodology.tsx
+++ src/components/Methodology.tsx
@@ -1,42 +1,42 @@
 import React from "react";
 import { OutboundLink } from "./Links";
+import { Changelog } from "./Changelog";
 
 export const Methodology = () => (
   <div className="copy">
-    <p className="mt-2">
+    <p className="mt-3">
       THE CITY and Gothamist chose a field of 12 candidates based on their poll
       standings, their presence at community forums and other appearances that
       provide a broad record of their positions and their history of public
       service. Most but not all candidates on the ballot are part of Meet Your
       Mayor.
     </p>
-    <p className="mt-2">
+    <p className="mt-3">
       In March, we sent each of the campaigns a multiple-choice survey asking
       them to make tough choices between pressing policy alternatives. We also
       checked their statements and stances against what the candidates have
       actually been saying on the campaign trail.
     </p>
-    <p className="mt-2">
-      Of the 12, only Eric Adams and Jim Walden did not respond to the survey.
-      Meet Your Mayor has assigned responses based on their policies and public
-      statements.
+    <p className="mt-3">
+      Of the 12, only Eric Adams did not respond to the survey. Meet Your Mayor
+      has assigned responses based on his policies and public statements.
     </p>
-    <p className="mt-2">
+    <p className="mt-3">
       In the quiz, candidates score 1 point for each question on which their
       answer matches the answer that a voter provides. A response of “no
       position” does not count toward a candidate’s score.
     </p>
-    <p className="mt-2">
+    <p className="mt-3">
       At the end of the quiz, voters select up to three issues of top interest,
       and each 1-point score in those categories counts as 2 points in the final
       tally.
     </p>
-    <p className="mt-2">
+    <p className="mt-3">
       Meet Your Mayor does not collect any personal data from users. We do
       collect quiz responses anonymously to improve this resource and track
       aggregate results.
     </p>
-    <p className="mt-2">
+    <p className="mt-3">
       As in 2021, we are pleased to make our code available on{" "}
       <OutboundLink to="https://github.com/thecityny/2025-meet-your-mayor">
         Github
@@ -56,5 +56,7 @@ export const Methodology = () => (
       </OutboundLink>
       .
     </p>
+    <p className="mt-3 has-text-weight-semibold">Updates</p>
+    <Changelog />
   </div>
 );
